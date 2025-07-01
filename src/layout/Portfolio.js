9883e76019efb9ea932b815e6edfa7da17ec67eb
Ashish Anand <ashishanand963@gmail.com>
2025-07-01 13:48:48 -0400

    lint

--- src/layout/Portfolio.js
+++ src/layout/Portfolio.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useRef, useEffect } from "react";
 import { Grid, Box } from "@mui/material";
 import SpaceBackground from "../components/SpaceBackground";
